55f00328ab08dda6a1751f2a7cea895f003280b5
José Cañizares <jose.canizares@colorado.edu>
2017-01-10 13:04:16 -0700

    Installed React Setup

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  render: function() {
+    return <div>Hello</div>;
+  }
+});
+
+module.exports = App;
