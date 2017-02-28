8c37fbed18a459d92fc06b2894f80fb336fdc1d5
José Cañizares <jose.canizares@colorado.edu>
2017-02-27 20:50:53 -0700

    Move inline CSS back to Sass files.

--- app/layout/PanelContainer.js
+++ app/layout/PanelContainer.js
@@ -30,26 +30,9 @@ var PanelContainer = React.createClass({
   },
   render: function() {
 
-    var PanelContainer = {
-      transform: 'scale3D($carousel-dots-scale, $carousel-dots-scale, $carousel-dots-scale)',
-      MozTransform: 'scale3D($carousel-dots-scale, $carousel-dots-scale, $carousel-dots-scale)',
-      WebkitTransform: 'scale3D($carousel-dots-scale, $carousel-dots-scale, $carousel-dots-scale)',
-      transition: 'all 0.25s ease-in-out',
-      MozTransition: 'all 0.25s ease-in-out',
-      WebkitTransition: 'all 0.25s ease-in-out',
-      paddingLeft: '50%',
-      width: '50%'
-    };
-
-    var PanelHover;
-    if (this.state.hoverPanelIndex != -1) {
-      PanelHover = <Panel clickedIndex={this.state.clickPanelIndex} key={this.state.hoverPanelIndex} name={'hoveredPanel'} onChange={this.handleChange} index={this.state.hoverPanelIndex} id={'focus'} clickPanelIndex={this.state.clickPanelIndex} sectionName={this.props.sectionName}></Panel>;
-    }
-
     var self = this; //enables me to pass this into a map function
     return (
-      <div style={PanelContainer} className={'PanelContainer'}>
-        {PanelHover}
+      <div className={'PanelContainer'}>
         {this.props.panels.map(function(panel, i) {return <Panel focused={0} key={i} name={panel} onChange={self.handleChange} index={i} id={panel} clickPanelIndex={self.state.clickPanelIndex} sectionName={self.props.sectionName}></Panel>})}
       </div>
       );
