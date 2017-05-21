5c38a7f913a09d2e121d7be07074cbf9c7f9b16a
José Cañizares <jose.canizares@colorado.edu>
2017-05-21 11:05:12 -0600

    create react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path='/home' component={Home}/>
+            <Route path='*' component={Home}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
