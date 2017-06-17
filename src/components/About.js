68faad6e0034f746b0a68f8199b0008fca7c74ce
jose.canizares <jose.canizares@workday.com>
2017-06-17 12:41:10 -0600

    create about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,14 @@ class About extends Component {
   render() {
     return (
       <div className='about-page'>
-        <div className='text-container'>
-          <h3>Vivefaux is a collaborative project based out of Boulder, Colorado.</h3>
-          <p></p>
+        <div className='section'>
+          <div className='section-title'>
+            <h1>About</h1>
+          </div>
+          <div className='text-block'>
+            <h3>A collaborative project based out of Boulder, Colorado.</h3>
+            <p>ViveFaux is a multimedia company formed in 2014 and based around the world. We are a group of several divisions including: Sōken Costa, Kamakura Records, the Blacq Label, and Infinite Vivacity Films.</p>
+          </div>
         </div>
       </div>
     );
