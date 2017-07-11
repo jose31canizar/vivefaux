38f992ff2fe9e12dc6c7ba5f1a4747985806aab7
jose.canizares <jose.canizares@workday.com>
2017-07-11 14:22:11 -0700

    center logo, add highlight to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,28 @@ import data from '../constants/navbar.json';
 import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: "Home"
+    }
+    this.setSelected = this.setSelected.bind(this)
+  }
+  setSelected(title) {
+    this.setState({
+      selected: title
+    })
+  }
   render() {
     return (
       <div className='nav-bar'>
         <ul>
           {data.map((item, i) => (
-            <Link className="nav-link" to={`/${item.route}`}>
-              <li className="nav-item-above">
+            <Link className="nav-link" to={`/${item.route}`} onMouseDown={this.setSelected.bind(this, item.title)}>
+              <li className={"nav-item-above "  + (item.title === this.state.selected ? "selected-page" : "")}>
                 {item.title}
               </li>
-              <li className="nav-item">
+              <li className={"nav-item " + (item.title === this.state.selected ? "selected-page" : "")} onMouseDown={this.setSelected.bind(this, item.title)}>
                 {item.title}
               </li>
             </Link>
